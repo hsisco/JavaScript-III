71b09f642d8e0e57c54fd8643e78a1ef6ae91ea9
hsisco <hysensisco@gmail.com>
2019-09-18 12:41:57 -0700

    Initial Upload, this MVP

--- assignments/this.js
+++ assignments/this.js
@@ -1,26 +1,44 @@
 /* The for principles of "this";
 * in your own words. explain the four principle for the "this" keyword below.
 *
-* 1. 
-* 2. 
-* 3. 
-* 4. 
+* 1. Global Binding - the value is the window or console presented.
+* 2. Implicit Binding - the object containing 'this'.
+* 3. New Binding - creates a new object out of a constructor function.
+* 4. Explicit Binding - groups methods using call / apply.
 *
 * write out a code example of each explanation above
 */
 
 // Principle 1
 
-// code example for Window Binding
+console.log(this)
 
 // Principle 2
 
-// code example for Implicit Binding
+const myObj = {
+  greeting: 'Hello',
+  speak: function(){
+      return `${this.greeting}, world!`
+  }
+};
+myObj.speak();
 
 // Principle 3
 
-// code example for New Binding
+function Person(obj){
+  this.name = obj.name;
+  this.age = obj.age;
+  this.speak = function(){
+      return `Hello, my name is ${this.name}, and I am ${this.age} years old!`
+  }
+}
+const hysen = new Person({name: 'Hysen', age: 31});
+const olivia = new Person({name: 'Olivia', age: 1});
+
+hysen.speak();
+olivia.speak();
 
 // Principle 4
 
-// code example for Explicit Binding
+hysen.speak.call(olivia)
+olivia.speak.apply(hysen);
